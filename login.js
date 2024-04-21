951eb02ff75e038d147bd63c48fbe8658e851210
ayadhs <167652223+ayadhs@users.noreply.github.com>
2024-04-22 00:23:54 +0100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,22 @@
+       
+		 
+		 
+		 
+		 
+
+function auth(event){
+	event.preventDefault();
+var Username = document.getElementById("Username").value;
+var Password = document.getElementById("assword").value;
+
+
+		{			
+	     if(name.value=='ayadahouas && Pass.value==12345'){
+		 
+		window.location.replace='pg2.html'};
+		}else{
+			alert("معلومات خاطئة");
+
+		}
+};
+
